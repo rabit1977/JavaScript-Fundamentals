f8c1e1746ed0c3f7e6e0731d4ec0b455ba2491b2
Rabit <rabit2245@gmail.com>
2021-12-05 15:29:40 +0100

    added template strings example

---
+++ template-strings/index.js
@@ -0,0 +1,8 @@
+const name = 'andi';
+const job = 'developer';
+
+const string = 'Hi my name is ' + name + ', and i am a deveiloper';
+const string1 = `Hi my name is  ${name}, and i am a ${job}`;
+
+console.log(string);
+console.log(string1);
